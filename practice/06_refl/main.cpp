9810ad81c6183e1f8b4b1b86d3f028c69beb6c73
Nebojša Koturović <contact@kotur.me>
2022-06-26 02:04:45 +0200

    Update refl example

--- practice/06_refl/main.cpp
+++ practice/06_refl/main.cpp
@@ -6,9 +6,36 @@
 #include <range/v3/view.hpp>
 #include <functional>
 #include <variant>
+#include <optional>
 
 #include <boost/hana.hpp>
 #include <boost/hana/adapt_struct.hpp>
+#include <boost/hana/optional.hpp>
+
+template <typename T>
+struct print_type;
+
+
+#define MTA_REFL BOOST_HANA_ADAPT_STRUCT
+#define MTA_STR BOOST_HANA_STRING
+
+namespace hana = boost::hana;
+namespace rng = ranges;
+namespace vw = ranges::views;
+
+
+constexpr auto to_std_tuple(auto hana_tuple) {
+    return hana::unpack(hana_tuple, [](auto ...xs) {
+        return std::make_tuple(xs...);
+    });
+}
+
+constexpr auto to_hana_tuple(auto std_tuple) {
+    return std::apply([]<typename ...T>(T&& ...args) {
+        return hana::make_tuple(args...);
+    }, std_tuple);
+}
+
 
 // for_each over tuple elements
 template <typename TupleT, typename Fn>
@@ -32,13 +59,6 @@ constexpr auto transform_tuple(TupleT&& tp, Fn&& fn) {
     );
 }
 
-
-#define MTA_REFL BOOST_HANA_ADAPT_STRUCT
-
-namespace hana = boost::hana;
-namespace rng = ranges;
-namespace vw = ranges::views;
-
 struct User {
     uint64_t id;
     std::string firstname;
@@ -50,11 +70,12 @@ MTA_REFL(User, id, firstname, lastname);
 // Reflection for classes
 namespace mta {
 
-template <typename Callable>
+template <typename ClassType, auto Name, auto MemPtr>
 struct FieldDescriptor {
-    // using value_type = 
-    std::string_view name;
-    Callable value_getter;
+    using class_type = ClassType;
+    using member_type = decltype(hana::typeid_(MemPtr))::type;
+    constexpr static std::string_view name = Name;
+    constexpr static const auto member_ptr = MemPtr;
 };
 
 // template <typename Callable>
@@ -62,11 +83,19 @@ struct FieldDescriptor {
 
 template <typename T>
 struct Refl {
-    consteval auto fields() const {
+    consteval static auto get_fields() {
         return hana::unpack(hana::transform(hana::accessors<T>(), [](auto x) {
-            return FieldDescriptor{.name = std::string_view{hana::first(x).c_str()}, 
-                                   .value_getter = [x](T& t) -> decltype(auto) { return hana::second(x)(t); }};
+            return FieldDescriptor<T, hana::first(x).c_str(),  
+                                   hana::second(x)>{};
         }),  []<class ...Xs>(Xs ...xs) { return std::tuple{xs...}; });
+    }
+
+    static consteval auto get_field(auto s) noexcept {
+        constexpr auto acs = hana::accessors<T>();
+        constexpr auto map = hana::unpack(acs, [](auto ...xs) {
+                return hana::make_map(xs...);
+        });
+        return FieldDescriptor<T, s.c_str(), map[s]>{};
     }
 };
 } // namespace mta
@@ -79,29 +108,32 @@ int main()
         .lastname = "Koturovic",
     };
 
-    constexpr static auto user_refl = mta::Refl<User>{};
-    constexpr static auto fields = user_refl.fields();
+    using user_refl = mta::Refl<User>;
+    constexpr static auto fields = user_refl::get_fields();
 
-    for_each_tuple(fields, [&u](auto fd) {
-        fmt::print("{} -> {}\n", fd.name, fd.value_getter(u));
-               //fd.value_getter(u) = "Shone";
+    for_each_tuple(fields, [&u]<typename F>(F) {
+        fmt::print("{} -> {}\n", F::name, F::member_ptr(u));
+               //f.member_ptr(u) = "Shone";
     });
 
-    constexpr static auto res = transform_tuple(fields, [](auto fd) {
-        // if constexpr (fd.name == std::string_view{"name"}) {
-        //      return std::string_view{"name"};
-        //} else {
-            return fd.name;
-       // }
+    constexpr static auto res = transform_tuple(fields, []<typename F>(F) {
+        if constexpr (F::name == std::string_view{"firstname"}) {
+              return std::string_view{"name"};
+        } else {
+            return F::name;
+       }
     });
 
     fmt::print("{}\n", res);
+    fmt::print("{}\n", user_refl::get_field(MTA_STR("firstname")).name);
+    constexpr auto ht = to_hana_tuple(res);
+    fmt::print("{}\n", to_std_tuple(ht));
 
     return 0;
 }
 
 // std::apply([&u](auto ...fs) { 
-//     (fmt::print("{} -> {}\n", fs.name, fs.value_getter(u)), ...);
+//     (fmt::print("{} -> {}\n", fs.name, fs.member_ptr(u)), ...);
 // }, fields);
 // ===========================
 // template <typename T>
